ee6d9e28154e9e4b30763bf91e8062c3bba29251
Illia Khovanskyi <saskiuhia@gmail.com>
2020-08-13 02:58:20 +0300

    Fix dockerEntryPoint.sh

--- docker/dockerEntryPoint.sh
+++ docker/dockerEntryPoint.sh
@@ -3,7 +3,7 @@ set -xe
 : "${URLS_GITHUB?URLS_GITHUB argument was not provided}"
 : "${URLS_DOCKER?URLS_DOCKER argument was not provided}"
 
-sed -i "s|URLS_GITHUB|$URLS_GITHUB|g" /usr/share/nginx/html/main-*.js
-sed -i "s|URLS_DOCKER|$URLS_DOCKER|g" /usr/share/nginx/html/main-*.js
+sed -i "s|URLS_GITHUB|$URLS_GITHUB|g" /usr/share/nginx/html/main*.js
+sed -i "s|URLS_DOCKER|$URLS_DOCKER|g" /usr/share/nginx/html/main*.js
 
 exec "$@"
